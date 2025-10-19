68592acc2276e8af57751c629ca82eeb858d36ca
[Prashant <[prashantnagulkar5@gmail.com]>
2025-10-19 16:56:29 +0530

    add Updateion Operation

--- src/arrayOperations.java
+++ src/arrayOperations.java
@@ -83,6 +83,21 @@
 //    }
 //}
 
+//Updation
+
+public class arrayOperations {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,50};
+        int element = 40;
+        int index = 3;
+
+        arr[index] = element;
+
+        for (int i= 0; i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
 
 
 
