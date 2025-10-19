123ddfc6db31e0be52eff11361ef500e535790fa
[Prashant <[prashantnagulkar5@gmail.com]>
2025-10-19 15:14:27 +0530

    Modify code

--- src/arrayOperations.java
+++ src/arrayOperations.java
@@ -66,24 +66,22 @@
 
 // Searching
 
-public class arrayOperations {
-    public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50};
-        int element = 30;
-        boolean found = false;
-        for (int i = 0; i< arr.length;i++){
-            if(arr[i] == element){
-                found=true;
-                break;
-            }
-        }
-        if (found){
-            System.out.println(true);
-        }else {
-            System.out.println("Invalid input");
-        }
-    }
-}
+//public class arrayOperations {
+//    public static void main(String[] args) {
+//        int[] arr = {10,20,30,40,50};
+//        int element = 30;
+//        boolean found = false;
+//        for (int i = 0; i< arr.length;i++){
+//            if(arr[i] == element){
+//                found=true;
+//                break;
+//            }
+//        }
+//        if (found == false){
+//            System.out.println("Element is not found");
+//        }
+//    }
+//}
 
 
 
