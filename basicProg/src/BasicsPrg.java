23c82eecfa0515cb5d7d940b45ce5bc553751dfb
[Prashant <[prashantnagulkar5@gmail.com]>
2025-10-19 17:14:52 +0530

    smallest element

--- basicProg/src/BasicsPrg.java
+++ basicProg/src/BasicsPrg.java
@@ -1,14 +1,31 @@
 // Find largest element in array
 
+//public class BasicsPrg {
+//    public static void main(String[] args) {
+//        int[] arr = {10,20,40,60,30};
+//        int max = arr[0];
+//        for (int i = 0; i< arr.length; i++){
+//            if(arr[i] > max){
+//                max = arr[i];
+//            }
+//        }
+//        System.out.println("Largest no is: "+max);
+//    }
+//}
+
+// Find Smallest element in array
+
 public class BasicsPrg {
     public static void main(String[] args) {
-        int[] arr = {10,20,40,60,30};
-        int max = arr[0];
-        for (int i = 0; i< arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
+        int[] arr ={10,2,40,70,20};
+        int min = arr[0];
+
+        for (int i =0; i< arr.length;i++){
+            if(arr[i]<min){
+                min = arr[i];
             }
         }
-        System.out.println("Largest no is: "+max);
+        System.out.println("smallest no is "+min);
     }
 }
+
