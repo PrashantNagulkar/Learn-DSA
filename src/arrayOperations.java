0b11059818e1901be41accf86261e4d1f2204837
[Prashant <[prashantnagulkar5@gmail.com]>
2025-10-19 15:03:52 +0530

    add deletion

--- src/arrayOperations.java
+++ src/arrayOperations.java
@@ -34,37 +34,56 @@
 
 //Insertion with index
 
-public class arrayOperations{
-    public static void main(String[] args){
-        int[] arr = {10,20,40,50};
-        int index = 2;
+//public class arrayOperations{
+//    public static void main(String[] args){
+//        int[] arr = {10,20,40,50};
+//        int index = 2;
+//        int element = 30;
+//
+//        // Create a new array with +1 size
+//        int[] arr2 = new int[arr.length+1];
+//
+//        //First print no 0 to index
+//        for (int i = 0; i<index;i++){
+//            arr2[i] = arr[i];
+//        }
+//
+//        //Initialize our element
+//        arr2[index] = element;
+//
+//        // after initialize continue next elements
+//        for (int i =index;i< arr.length;i++){
+//            arr2[i+1] =arr[i];
+//        }
+//
+//        //Print all element with new array
+//        for (int i = 0; i < arr2.length; i++) {
+//            System.out.println(arr2[i]);
+//        }
+//    }
+//}
+
+
+// Searching
+
+public class arrayOperations {
+    public static void main(String[] args) {
+        int[] arr = {10,20,30,40,50};
         int element = 30;
-
-        // Create a new array with +1 size
-        int[] arr2 = new int[arr.length+1];
-
-        //First print no 0 to index
-        for (int i = 0; i<index;i++){
-            arr2[i] = arr[i];
+        boolean found = false;
+        for (int i = 0; i< arr.length;i++){
+            if(arr[i] == element){
+                found=true;
+                break;
+            }
         }
-
-        //Initialize our element
-        arr2[index] = element;
-
-        // after initialize continue next elements
-        for (int i =index;i< arr.length;i++){
-            arr2[i+1] =arr[i];
-        }
-
-        //Print all element with new array
-        for (int i = 0; i < arr2.length; i++) {
-            System.out.println(arr2[i]);
+        if (found){
+            System.out.println(true);
+        }else {
+            System.out.println("Invalid input");
         }
     }
 }
-
-
-//Deletion
 
 
 
