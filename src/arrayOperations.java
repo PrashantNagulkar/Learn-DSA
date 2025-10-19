2f63a2d7d294856be82977797536c3d7db79d77e
[Prashant <[prashantnagulkar5@gmail.com]>
2025-10-19 14:38:35 +0530

    insertion operation

---
+++ src/arrayOperations.java
@@ -0,0 +1,73 @@
+// Traverse Array
+
+//public class arrayOperations {
+//    public static void main(String[] args) {
+//        int[] arr = {102,30,40,50};
+//        for (int i = 0; i < arr.length; i++) { // Accessing elements one by one
+//            System.out.print(arr[i]+" "); //Print all the element of array
+//        }
+//    }
+//}
+
+
+//Insertion withought index
+
+//public class arrayOperations {
+//    public static void main(String[] args) {
+//        int[] arr = {10,20,30,40};
+//        int element = 50;
+//        int[] arr2 = new int[arr.length+1];
+//
+//
+//        for (int i = 0; i < arr.length; i++){
+//            arr2[i] = arr[i];
+//        }
+//
+//        arr2[arr2.length-1] = element;
+//
+//        for (int i = 0; i< arr2.length;i++){
+//            System.out.println(arr2[i]);
+//        }
+//    }
+//}
+
+
+//Insertion with index
+
+public class arrayOperations{
+    public static void main(String[] args){
+        int[] arr = {10,20,40,50};
+        int index = 2;
+        int element = 30;
+
+        // Create a new array with +1 size
+        int[] arr2 = new int[arr.length+1];
+
+        //First print no 0 to index
+        for (int i = 0; i<index;i++){
+            arr2[i] = arr[i];
+        }
+
+        //Initialize our element
+        arr2[index] = element;
+
+        // after initialize continue next elements
+        for (int i =index;i< arr.length;i++){
+            arr2[i+1] =arr[i];
+        }
+
+        //Print all element with new array
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
+        }
+    }
+}
+
+
+//Deletion
+
+
+
+
+
+
