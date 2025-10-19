63b04b4da94254cf4658d4aee9c36d469b6c09ad
[Prashant <[prashantnagulkar5@gmail.com]>
2025-10-19 17:29:35 +0530

    sum all element and sencond largest element

--- basicProg/src/BasicsPrg.java
+++ basicProg/src/BasicsPrg.java
@@ -15,17 +15,49 @@
 
 // Find Smallest element in array
 
+//public class BasicsPrg {
+//    public static void main(String[] args) {
+//        int[] arr ={10,2,40,70,20};
+//        int min = arr[0];
+//
+//        for (int i =0; i< arr.length;i++){
+//            if(arr[i]<min){
+//                min = arr[i];
+//            }
+//        }
+//        System.out.println("smallest no is "+min);
+//    }
+//}
+
+// sum of all elemnet
+
+//public class BasicsPrg {
+//    public static  void main(String[] args) {
+//        int[] arr = {10,30,40,50};
+//        int sum = 0;
+//
+//        for (int i=0; i< arr.length;i++){
+//            sum += arr[i];
+//        }
+//    }
+//}
+
+// 2nd largest no
+
 public class BasicsPrg {
     public static void main(String[] args) {
-        int[] arr ={10,2,40,70,20};
-        int min = arr[0];
+        int[] arr = {10,20,30,60,80};
+        int largest = Integer.MIN_VALUE;
+        int secondl = Integer.MIN_VALUE;
 
-        for (int i =0; i< arr.length;i++){
-            if(arr[i]<min){
-                min = arr[i];
+        for (int i = 0; i<arr.length;i++){
+            if(arr[i] > largest){
+                secondl = largest;
+                largest = arr[i];
+            } else if (arr[i]>secondl && secondl!=largest ) {
+                secondl = arr[i];
             }
         }
-        System.out.println("smallest no is "+min);
+        System.out.println("second largest element is: "+secondl);
     }
 }
-
